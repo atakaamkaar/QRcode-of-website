58008f58c524e2d89c3604ef3d802430a98e557c
wubalabaaduuubduuuuuub <drkaamkaar76@gmail.com>
2024-07-10 18:03:51 +0330

    Add files via upload

---
+++ qrcode.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# Data to be encoded
+data = "www.strata360sphere.com"  # Replace this with the actual data from your QR code
+
+# Create QR code instance
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Add data to the QR code
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill='black', back_color='white')
+
+# Save the image
+img.save("qrcode.png")
